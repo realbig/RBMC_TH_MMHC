f2958606e2ef0b868e9ded49464d31f3d27398e6
joelworsham <joelworsham@gmail.com>
2015-03-26 11:03:49 -0400

    Update site map...again!

--- templates/site-map.php
+++ templates/site-map.php
@@ -2,7 +2,7 @@
 /**
  * Template Name: Site Map.
  *
- * @since 1.0.0
+ * @since   1.0.0
  * @package MMHC
  */
 
@@ -14,52 +14,68 @@ if ( ! defined( 'ABSPATH' ) ) {
 get_header();
 ?>
 
-<h2 id="authors">Authors</h2>
-<ul>
-	<?php
-	wp_list_authors(
-		array(
-			'exclude_admin' => false,
-		)
-	);
-	?>
-</ul>
+	<div class="row">
 
-<h2 id="pages">Pages</h2>
-<ul>
-	<?php
-	// Add pages you'd like to exclude in the exclude here
-	wp_list_pages(
-		array(
-			'exclude' => '',
-			'title_li' => '',
-		)
-	);
-	?>
-</ul>
+		<div id="site-map" class="columns small-12 medium-8">
 
-<h2 id="posts">Posts</h2>
-<ul>
-	<?php
-	// Add categories you'd like to exclude in the exclude here
-	$cats = get_categories('exclude=');
-	foreach ($cats as $cat) {
-		echo "<li><h3>".$cat->cat_name."</h3>";
-		echo "<ul>";
-		query_posts('posts_per_page=-1&cat='.$cat->cat_ID);
-		while(have_posts()) {
-			the_post();
-			$category = get_the_category();
-			// Only display a post link once, even if it's in multiple categories
-			if ($category[0]->cat_ID == $cat->cat_ID) {
-				echo '<li><a href="'.get_permalink().'">'.get_the_title().'</a></li>';
-			}
-		}
-		echo "</ul>";
-		echo "</li>";
-	}
-	?>
-</ul>
+			<h1 class="page-title">
+				<?php the_title(); ?>
+			</h1>
+
+			<div class="page-content">
+				<h2 id="authors">Authors</h2>
+				<ul>
+					<?php
+					wp_list_authors(
+						array(
+							'exclude_admin' => false,
+						)
+					);
+					?>
+				</ul>
+
+				<h2 id="pages">Pages</h2>
+				<ul>
+					<?php
+					// Add pages you'd like to exclude in the exclude here
+					wp_list_pages(
+						array(
+							'exclude'  => '',
+							'title_li' => '',
+						)
+					);
+					?>
+				</ul>
+
+				<h2 id="posts">Posts</h2>
+				<ul>
+					<?php
+					// Add categories you'd like to exclude in the exclude here
+					$cats = get_categories( 'exclude=' );
+					foreach ( $cats as $cat ) {
+						echo "<li><h3>" . $cat->cat_name . "</h3>";
+						echo "<ul>";
+						query_posts( 'posts_per_page=-1&cat=' . $cat->cat_ID );
+						while ( have_posts() ) {
+							the_post();
+							$category = get_the_category();
+							// Only display a post link once, even if it's in multiple categories
+							if ( $category[0]->cat_ID == $cat->cat_ID ) {
+								echo '<li><a href="' . get_permalink() . '">' . get_the_title() . '</a></li>';
+							}
+						}
+						echo "</ul>";
+						echo "</li>";
+					}
+					?>
+				</ul>
+			</div>
+
+		</div>
+
+		<?php get_sidebar(); ?>
+
+	</div>
 
 <?php
 get_footer();
